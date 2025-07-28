91a9d7861fb56c65c30afaec94f67908e45b494d
Shreyas Kotian <shreyaskotian09@gmail.com>
2025-07-28 12:17:18 +0530

    Generic types on Structs

--- handling-duplication/src/main.rs
+++ handling-duplication/src/main.rs
@@ -1,3 +1,17 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
 fn get_largest<T: PartialOrd + Copy>(list: Vec<T>) -> T {
     let mut largest = list[0];
 
@@ -20,4 +34,11 @@ fn main() {
 
     let largest = get_largest(char_list);
     println!("The largest char is {largest}");
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
